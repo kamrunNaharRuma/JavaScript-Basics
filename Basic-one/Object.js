12e5dcedf6e27f8f7245c61752cf6dfaca946abe
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-30 01:17:18 +0600

    Object

---
+++ Basic-one/Object.js
@@ -0,0 +1,18 @@
+//reference type : Object , Array, Function
+//
+
+let person ={
+
+}; //object literal
+
+let book = {//Key value pair
+author   : 'ruma'//keys are properties of the object
+};
+
+console.log(book);
+//Dot notation
+book.author ="nahar";
+console.log(book);
+//Bracket Notation
+book["author"] = "kamrun";
+console.log(book);
