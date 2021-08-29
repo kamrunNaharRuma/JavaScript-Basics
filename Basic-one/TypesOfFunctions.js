c5593d1d8b4da72be60548229b7cb0b603b09cdd
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-30 01:30:54 +0600

    functionss

---
+++ Basic-one/TypesOfFunctions.js
@@ -0,0 +1,17 @@
+function greet(){
+
+}
+//Performing a task
+function sendMessage(name){  //parameter
+    console.log('hello ' + name);
+}
+
+
+//Calculating a value
+function square(number){
+    return number * number;
+}
+sendMessage('Ruma'); //argument
+let num = square(2);
+
+console.log(num);
