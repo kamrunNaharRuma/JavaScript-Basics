f23f2b39be10b5ffd608d10c35ebdb3fb9deaf0f
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-29 22:25:44 +0600

    variables

---
+++ Basic-one/variables.js
@@ -0,0 +1,8 @@
+//we use a variable to store a data temporarily in a computer's memory
+//ES6
+
+let name = 'Kamrun Nahar Ruma';
+console.log(name);
+
+//cant be a reserved keyword
+//case sensitive
