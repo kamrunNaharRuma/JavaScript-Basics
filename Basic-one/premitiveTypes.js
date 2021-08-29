a80902591ced9191ad574ab3dd23f64e470c1dba
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-29 23:34:07 +0600

    premitiveTypes

---
+++ Basic-one/premitiveTypes.js
@@ -0,0 +1,10 @@
+//there are two categories of types
+//1. primitives
+//2. reference
+
+let name = "ruma"; //String Literal
+let age = 30; //Number Literal
+let isHere = true; //Boolean Literal
+
+let firstName; //undefined
+let lastName = null;
