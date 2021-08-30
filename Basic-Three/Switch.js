12cd3d3546f7c464647b1ae96959dfdce2f3693a
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-30 20:14:26 +0600

    switch

---
+++ Basic-Three/Switch.js
@@ -0,0 +1,15 @@
+let role = 'engineer'
+
+switch (role) {
+    case 'guest':
+        console.log("Guest User");
+        break;
+    case 'moderator':
+        console.log("Moderate User");
+        break;
+    case 'engineer':
+        console.log("Engineer")
+        break;
+    default:
+        console.log("default user");
+}
