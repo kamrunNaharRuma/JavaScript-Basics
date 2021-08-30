43019554ed15bee44fb4d1d60337ec1896c39d5e
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-30 20:14:41 +0600

    InfiniteLoop

---
+++ Basic-Three/InfiniteLoop.js
@@ -0,0 +1,17 @@
+let i=0;
+while(i<5) {
+    if(count % 2 !== 0 ){
+        console.log(count);
+    }
+    // i++;
+}
+
+while (true){
+
+}
+
+do{
+
+}while (i <10);
+
+//with no increment and decrement
