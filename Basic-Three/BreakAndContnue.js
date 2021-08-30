714b4db12e5df132b73a17e47e1c084b8629ce63
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-30 20:15:25 +0600

    Break and continue

---
+++ Basic-Three/BreakAndContnue.js
@@ -0,0 +1,18 @@
+let i = 0;
+
+// while(i < 10){
+//     if(i===5){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+while(i < 10){
+    if(i===6){
+        i++;
+        continue;
+    }
+    console.log(i);
+    i++;
+}
