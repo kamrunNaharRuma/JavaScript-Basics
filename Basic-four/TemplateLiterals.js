0f717a76ed1d00e99460a32ce02f7277bb8ebe87
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-30 22:48:00 +0600

    template

---
+++ Basic-four/TemplateLiterals.js
@@ -0,0 +1,17 @@
+const message = 'hi \n there';  //String Primitive
+
+//String Object
+
+const another = new String('h1 ');
+
+another.includes('hi');
+
+console.log(message);
+
+const anotherLine =
+    `this is my 'first' 
+     letter
+${message}`;
+
+
+console.log(anotherLine);
