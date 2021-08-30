38e47ddcf7e75d318b585410e7517d3f03e2155c
kamrunnaharruma <knahar12bd@gmail.com>
2021-08-30 22:48:53 +0600

    Functions are object

---
+++ Basic-four/FunctionsAreObjects.js
@@ -0,0 +1,10 @@
+function Circle(radius){
+    this.radius = radius;
+}
+
+const circle1 = new Circle(12);
+
+console.log(Circle.name);
+console.log(Circle.length);
+console.log(Circle.constructor);
+console.log(circle1);
