45a5335aec15bed9bda33f952188fac2747b28b3
kamrunnaharruma <knahar12bd@gmail.com>
2021-09-14 20:45:41 +0600

    Add Methods After Inheritance

---
+++ Practices/BasicOOP/AddMethodsAfterInheritance.js
@@ -0,0 +1,21 @@
+//Add Methods After Inheritance
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function(){
+    console.log("Woof!");
+}
+
+
+
+// Only change code above this line
+
+let beagle = new Dog();
+
+console.log(beagle.bark())
