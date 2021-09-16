ab49ab0d0d1096b0fb396847844249a5e276da74
kamrunnaharruma <knahar12bd@gmail.com>
2021-09-16 22:42:49 +0600

    functional programming

---
+++ FunctionalProgramming/every.js
@@ -0,0 +1,9 @@
+function checkPositive(arr) {
+    // Only change code below this line
+
+    return arr.every((currentValue) => {
+        return currentValue >0;
+    });
+    // Only change code above this line
+}
+console.log(checkPositive([1, 2, 3, -4, 5]));
