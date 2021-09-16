cbb33833494c6c50412942936db265c02e6a50b1
kamrunnaharruma <knahar12bd@gmail.com>
2021-09-16 22:41:40 +0600

    array methods

---
+++ ArrayMethods/indexOf.js
@@ -0,0 +1,4 @@
+let a = [1, 2, 3, 4, 5, 6 , 7, 8];
+
+console.log(a.indexOf(1))
+console.log(a.indexOf(21))
---
+++ ArrayMethods/key.js
@@ -0,0 +1,6 @@
+let a = [1, 2, 3, 4, 5, 6 , 7, 8];
+let keyss = a.keys();
+
+for(let key of keyss){
+    console.log(key)
+}
