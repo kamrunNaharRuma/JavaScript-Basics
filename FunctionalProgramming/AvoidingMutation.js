c1385e5f1ff5921afdf0d1203df5714f934faeb6
kamrunnaharruma <knahar12bd@gmail.com>
2021-09-16 14:42:14 +0600

    im-mutaion

---
+++ FunctionalProgramming/AvoidingMutation.js
@@ -0,0 +1,13 @@
+///AVOID MUTATIONS
+
+
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+    // Only change code below this line
+
+    let incremented = fixedValue + 1;
+    return incremented;
+    // Only change code above this line
+}
