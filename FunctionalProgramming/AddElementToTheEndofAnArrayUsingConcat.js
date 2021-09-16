b187be1605b0324904178f1e8cceb870755abf0e
kamrunnaharruma <knahar12bd@gmail.com>
2021-09-16 14:41:37 +0600

    conacat

---
+++ FunctionalProgramming/AddElementToTheEndofAnArrayUsingConcat.js
@@ -0,0 +1,12 @@
+/////AddElementToTheEndofAnArrayUsingConcat.js
+function nonMutatingPush(original, newItem) {
+    // Only change code below this line
+    return original.concat(newItem)
+
+    // Only change code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+console.log(nonMutatingPush(first, second));
+
+console.log(first);
