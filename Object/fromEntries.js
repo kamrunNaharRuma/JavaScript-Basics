299c23a8895aeff144c46f638a07951fb566b87f
kamrunnaharruma <knahar12bd@gmail.com>
2021-09-24 13:22:56 +0600

    object methods

---
+++ Object/fromEntries.js
@@ -0,0 +1,4 @@
+const entries = [["age", 18], ["school", "MMHS"]]
+const obj = Object.fromEntries(entries)
+
+console.log(obj)
---
+++ Object/values.js
@@ -0,0 +1,7 @@
+const obj ={
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+console.log(Object.values(obj))
